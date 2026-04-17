814be2300a40adc32a9044a671aba237542b9505
jakariaistauk <jakariamd35@gmail.com>
2026-04-17 10:03:56 +0600

    ✨ config(vite): set build output directory to docs
    - change build output directory to 'docs' for deployment needs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/hajjflow/',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 5173,
   },
